361edf9381378a4da896d249a606886f2142c68c
lerstice <lerstice@gmail.com>
2021-05-25 22:58:03 +0300

    finish

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function getMonth(n) {
-	if (n === 1) return 'январь';
-	if (n === 2) return 'февраль';
-	if (n === 3) return 'март';
-	if (n === 4) return 'апрель';
-	if (n === 5) return 'май';
-	if (n === 6) return 'июнь';
-	if (n === 7) return 'июль';
-	if (n === 8) return 'август';
-	if (n === 9) return 'сентябрь';
-	if (n === 10) return 'октябрь';
-	if (n === 11) return 'ноябрь';
-	if (n === 12) return 'декабрь';
-  return 'неизвестно';
+
+	if (n === 1) return 'January';
+	if (n === 2) return 'February';
+	if (n === 3) return 'March';
+	if (n === 4) return 'April';
+	if (n === 5) return 'May';
+	if (n === 6) return 'June';
+	if (n === 7) return 'July';
+	if (n === 8) return 'August';
+	if (n === 9) return 'September';
+	if (n === 10) return 'October';
+	if (n === 11) return 'November';
+	if (n === 12) return 'December';
+	
+  return 'Not found';
 }
 
 module.exports = getMonth;
--- index.test.js
+++ index.test.js
@@ -1,6 +1,19 @@
+// const { describe } = require('yargs');
 const getMonth = require('./index.js');
 
-test ('adds 1 + 2 to equal 3', () => {
-  const result = getMonth(3);
-  expect(result).toBe('март');
+describe ('test month', () => {
+
+  it ('add 3 to receive March, add 10 to receive October', () => {
+    expect(getMonth(3)).toBe('March');
+    expect(getMonth(10)).toBe('October');
+  }),
+
+  it ('add 50 to receive Not found', () => {
+    expect(getMonth(50)).toBe('Not found');
+  });
+
+  it ('add text to receive Not found', () => {
+    expect(getMonth('sddd')).toBe('Not found');
+  });
+
 });
